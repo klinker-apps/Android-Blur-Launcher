a79c44d5a4a3fa12c226ea85dfa3ebc85df51476
Luke Klinker <lklinker1@gmail.com>
2015-10-08 19:55:34 -0500

    [Improve] Disable the row selector in the all apps grid preference

--- app/src/main/java/com/klinker/android/launcher/addons/view/DrawerGridSizePreference.java
+++ app/src/main/java/com/klinker/android/launcher/addons/view/DrawerGridSizePreference.java
@@ -10,13 +10,9 @@ import android.view.View;
 import com.klinker.android.launcher.R;
 import net.simonvt.numberpicker.NumberPicker;
 
-/**
- * Created by luke on 8/10/14.
- */
 public class DrawerGridSizePreference extends DialogPreference {
 
     private NumberPicker widthPicker;
-    private NumberPicker heightPicker;
 
     public DrawerGridSizePreference(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -26,9 +22,8 @@ public class DrawerGridSizePreference extends DialogPreference {
     public View onCreateDialogView() {
         LayoutInflater inflater = (LayoutInflater) getContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View preference = inflater.inflate(R.layout.preference_grid_size, null);
+        View preference = inflater.inflate(R.layout.preference_grid_size_all_apps, null);
         widthPicker = (NumberPicker) preference.findViewById(R.id.width_picker);
-        heightPicker = (NumberPicker) preference.findViewById(R.id.height_picker);
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
         widthPicker.setMaxValue(9);
@@ -39,13 +34,6 @@ public class DrawerGridSizePreference extends DialogPreference {
         );
         widthPicker.setWrapSelectorWheel(false);
 
-        heightPicker.setMaxValue(9);
-        heightPicker.setMinValue(3);
-        heightPicker.setValue(Integer.parseInt(
-                        sharedPrefs.getString("row_count_all_apps",
-                                getContext().getResources().getInteger(R.integer.default_row_count) + ""))
-        );
-        heightPicker.setWrapSelectorWheel(false);
         return preference;
     }
 
@@ -55,7 +43,6 @@ public class DrawerGridSizePreference extends DialogPreference {
             SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
             sharedPreferences.edit()
                     .putString("col_count_all_apps", widthPicker.getValue() + "")
-                    .putString("row_count_all_apps", heightPicker.getValue() + "")
                     .commit();
         }
     }
